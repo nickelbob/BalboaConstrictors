1cbfc17866106b7db8fd484bc8ede4e821b37788
Nick Peterson <nickuwo@gmail.com>
2013-03-06 08:07:18 -0800

    RSSI Updates
    RSSI Updates - Write to file, Write SQL to file, Help message, change
    interval

--- RSSIRecorder/RSSIRecorder/RSSIRecorder/Program.cs
+++ RSSIRecorder/RSSIRecorder/RSSIRecorder/Program.cs
@@ -13,10 +13,28 @@ namespace RSSIRecorder
     {
         //static RSSIRecorderEntities entity;
         static WlanClient client;
-        static bool useDatabase = true;
-        static string filename = String.Empty;
+        static bool useDatabase = true, outputSQL = false;
+        static string fileName = String.Empty, tableName = "RSSIRecorder";
+        static int pingInterval = 2000;
 
         static void Main(string[] args)
+        {
+            parseCmdLine(args);
+            
+            //entity = new RSSIRecorderEntities();
+            client = new WlanClient();
+            Console.WriteLine("Main thread: starting to ping -");
+
+            while (true)
+            {
+                Timer t = new Timer(getRSSI, 5, 0, pingInterval);
+                Console.WriteLine("Main thread: Continuing ping...");
+                Thread.Sleep(10000); // Simulating other work (10 seconds)
+                t.Dispose(); // Cancel the timer now
+            }
+        }
+
+        private static void parseCmdLine(string[] args)
         {
             var cmdline = args.ToList();
 
@@ -30,18 +48,38 @@ namespace RSSIRecorder
                     return;
                 }
 
-                filename = cmdline[cmdline.IndexOf("-f") + 1];
+                fileName = cmdline[cmdline.IndexOf("-f") + 1];
             }
-            //entity = new RSSIRecorderEntities();
-            client = new WlanClient();
-            Console.WriteLine("Main thread: starting a timer");
 
-            while (true)
+            if (cmdline.Any(x => x == "-t"))
             {
-                Timer t = new Timer(getRSSI, 5, 0, 2000);
-                Console.WriteLine("Main thread: Doing other work here...");
-                Thread.Sleep(10000); // Simulating other work (10 seconds)
-                t.Dispose(); // Cancel the timer now
+                outputSQL = true;
+
+                if (cmdline.Count() >= cmdline.IndexOf("-t") + 1)
+                {
+                    tableName = cmdline[cmdline.IndexOf("-t") + 1];   
+                }
+            }
+
+            if (cmdline.Any(x => x == "-i"))
+            {
+                if (cmdline.Count() < cmdline.IndexOf("-i") + 1)
+                {
+                    Console.WriteLine("Please supply a number of milliseconds after the '-i' flag to use as a ping interval.");
+                    return;
+                }
+
+                if (!Int32.TryParse(cmdline[cmdline.IndexOf("-f") + 1], out pingInterval))
+                {
+                    Console.WriteLine("Ping interval parsing was not successful. Using 2000 milliseconds as default.");
+                }
+
+            }
+
+            if (cmdline.Any(x => x == "-h"))
+            {
+                Console.WriteLine("Usage: The RSSI Recorder will print out the SSID, time stamp, and the signal strength of the WiFi network you are currently connected to.");
+                Console.WriteLine("FLAGS\n-f <filename> - By default the program will write to the DB pointed to in the exe.config file but supplying the -f option will write to a file instead.\n-t <table name> - The t flag only works if the f flag is passed as well. The t flag will print SQL insert statements to a file for later import into a DB of your choosing. Some modification may be neccessary if your DB is not MSSQL Server. Your table rows must be in this order - SSID SignalStrength Date.\n-i <milliseconds> - The i flag allows you to control the polling interval. 2000 (2 sec) is the default.");
             }
         }
 
@@ -57,6 +95,8 @@ namespace RSSIRecorder
                 string ssidName = new String(Encoding.ASCII.GetChars(ssid.SSID, 0, (int)ssid.SSIDLength));
                 connectedSsids.Add(ssidName);
 
+                Console.WriteLine(String.Format("{0}\t{1}\t{2}", ssidName, wlanInterface.RSSI.ToString(), DateTime.Now.ToString("o")));
+
                 if (useDatabase)
                 {
                     entity.RSSIRecords.Add(new RSSIRecord()
@@ -70,9 +110,16 @@ namespace RSSIRecorder
                 }
                 else
                 {
-                    using (System.IO.StreamWriter file = new System.IO.StreamWriter(filename, true))
+                    using (System.IO.StreamWriter file = new System.IO.StreamWriter(fileName, true))
                     {
-                        file.WriteLine(String.Format("{0}\t{1}\t{2}", ssidName, wlanInterface.RSSI.ToString(), DateTime.Now.ToString("o")));
+                        if (outputSQL)
+                        {
+                            file.WriteLine(String.Format("insert into [{0}] values('{1}', {2}, '{3}');", tableName, ssidName, wlanInterface.RSSI.ToString(), DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.ffff")));
+                        }
+                        else
+                        {
+                            file.WriteLine(String.Format("{0}\t{1}\t{2}", ssidName, wlanInterface.RSSI.ToString(), DateTime.Now.ToString("o")));
+                        }
                     }  
                 }
             }
